09fd1cd2886cf51ad707915eee850614aa0f2f1f
Tanushree Manjunath <tanushreemanjunath326@gmail.com>
2025-11-19 18:30:41 +1400

    Update app.js

--- server/app.js
+++ server/app.js
@@ -25,7 +25,7 @@ app.use("/api/loans", require("./routes/loanRoutes"));
 
 // For any other route, serve index.html (handle SPA or frontend routing)
 app.use((req, res) => {
-  res.sendFile(path.join(__dirname, "../public/index.html"));
+  res.sendFile(path.join(__dirname, "../public/student-login.html"));
 });
 
 app.listen(3000, () => console.log("Server running on port 3000"));
